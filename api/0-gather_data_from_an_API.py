88a76a0160e93cd2afeef734603c120fe2406230
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-15 15:03:03 -0400

    Adding redme and task1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,49 +1,28 @@
 #!/usr/bin/python3
-"""
-This module uses Python to make requests to a REST API.
-fix
-It fetches data about a specific employee's tasks
-and prints a summary of the tasks completed and the
-titles of the completed tasks.
-"""
+
+import json
 import requests
 import sys
 
-
-# Get the employee ID from the command line arguments
 employee_id = sys.argv[1]
 
-# Send a GET request to the API to get the user data
-user_response = requests.get(
-    'https://jsonplaceholder.typicode.com/users/' + employee_id)
+employee_data = requests.get('https://jsonplaceholder.typicode.com/users/' + employee_id)
+employee_data_json = employee_data.json()
 
-# Parse the response data as JSON
-data = user_response.json()
+employee_name = employee_data_json['name']
 
-# Extract the employee's name from the data
-employee_name = data['name']
+todo_data = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
+todo_data_json = todo_data.json()
 
-# Send another GET request to the API to get the todo data
-todos_response = requests.get(
-    'https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
+total_todos = str(len(todo_data_json))
+completed_todos = str(sum(1 for task in todo_data_json if task ['completed']))
 
-# Parse the todo data as JSON
-todos_data = todos_response.json()
-
-# Calculate the total number of tasks
-total_todos = str(len(todos_data))
-
-# Calculate the number of completed tasks
-completed_todos = str(sum(1 for task in todos_data if task['completed']))
-
-# Print the first line of the output
 print("Employee " + employee_name + " is done with tasks(" +
       completed_todos + "/" + total_todos + "):")
 
-# Print the title of each completed task
-for task in todos_data:
-    if task['completed']:
-        print('\t ' + task['title'])
+for task in todo_data_json:
+      if task['completed']:
+            print('\t ' + task['title'])
 
 if __name__ == '__main__':
     pass
