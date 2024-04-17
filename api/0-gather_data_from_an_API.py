4cec7020a4e68336b3ad3071d28a87ad45068d22
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-04-17 12:14:13 -0400

    Commiting first task after changes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
+"""
+This script uses the JSONPlaceholder API to fetch data about an employee's TODO list progress.
+
+It accepts an employee ID as a command-line argument and prints the employee's TODO list progress to the standard output.
+
+Usage:
+    python3 0-gather_data_from_an_API.py <employee_id>
+
+Arguments:
+    employee_id: An integer that represents the employee's ID.
+
+Output:
+    Prints the employee's TODO list progress in the following format:
+    Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+    Followed by the titles of the completed tasks, each on a new line.
+
+Modules used:
+    json: This module is used to parse the JSON data returned by the API.
+    requests: This module is used to make HTTP requests to the API.
+    sys: This module is used to read the command-line arguments.
+"""
 import json
 import requests
 import sys
 
-'''Converts the argument passed to the script to an integer and uses it to'''
+
 employee_id = int(sys.argv[1])
-'''Makes a request to the JSONPlaceholder API to get the employee data'''
+
 employee_data = requests.get('https://jsonplaceholder.typicode.com/users/' + employee_id)
 employee_data_json = employee_data.json()
 
